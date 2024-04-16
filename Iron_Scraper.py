119e419b2694c0e40905fbe7a0b252ddae4a2b94
Amin Achak <82500146+Clancyov@users.noreply.github.com>
2024-04-16 22:50:00 +0330

    Update Iron_Scraper.py

--- Iron_Scraper.py
+++ Iron_Scraper.py
@@ -26,51 +26,51 @@ class Iron_Scraper :
         URL = "https://www.iranjib.ir/showgroup/38/%D9%82%DB%8C%D9%85%D8%AA-%D8%A2%D9%87%D9%86-%D8%A2%D9%84%D8%A7%D8%AA/"
 
         #Scraping
-        # try:
+        try:
             # saving html code of the page
         HTML_Code = urlopen(URL).read().decode("utf-8")
-        # except:
-            # Logger.critical('Scrapping Faild')
-        # else:
-            # Logger.info('Data Scaraped Successfully')
+        except:
+            Logger.critical('Scrapping Faild')
+        else:
+            Logger.info('Data Scaraped Successfully')
 
         #Pre process
-        try:
-            # making a soup of the html code to handle it easier
-            Main_Soup = BeautifulSoup(HTML_Code, 'lxml')
+        # try:
+        # making a soup of the html code to handle it easier
+        Main_Soup = BeautifulSoup(HTML_Code, 'lxml')
 
-            # finding all tables in the page
-            All_Tables = Main_Soup.find_all("table")
+        # finding all tables in the page
+        All_Tables = Main_Soup.find_all("table")
 
-            # choosing the table which we want
-            Price_Table_Soup = BeautifulSoup(str(All_Tables[2]), 'lxml')
+        # choosing the table which we want
+        Price_Table_Soup = BeautifulSoup(str(All_Tables[2]), 'lxml')
 
-            # the needed data will be save in this array 
-            All_Data = []
+        # the needed data will be save in this array 
+        All_Data = []
 
-            # making a soup of all rows in the the table.
-            Rows = Price_Table_Soup.find_all("tr")
+        # making a soup of all rows in the the table.
+        Rows = Price_Table_Soup.find_all("tr")
 
-            # iterating between rows and cells to extracting data
-            for Row in Rows :
+        # iterating between rows and cells to extracting data
+        for Row in Rows :
 
-                # making a soup of each row to find data in html code
-                Row_Soup = BeautifulSoup(str(Row), 'lxml')
+            # making a soup of each row to find data in html code
+            Row_Soup = BeautifulSoup(str(Row), 'lxml')
 
-                # making a soup of cells in a row
-                Cell = Row_Soup.find_all("td")
+            # making a soup of cells in a row
+            Cell = Row_Soup.find_all("td")
 
-                # deleting the tale
-                if len(Cell) <= 1 :
-                    continue
+            # deleting the tale
+            if len(Cell) <= 1 :
+                continue
                 
-                # appending data to the holder array
-                for Data in Cell :
-                    All_Data.append(Data.get_text() + "\n")
-        except:
-            Logger.critical('Something Went Wrong About Scraped Data')
-        else:
-            Logger.info('Scrapped Data Has No Problem')
+            # appending data to the holder array
+            for Data in Cell :
+                All_Data.append(Data.get_text() + "\n")
+        # except:
+        #     Logger.critical('Something Went Wrong About Scraped Data')
+        # else:
+        #     Logger.info('Scrapped Data Has No Problem')
 
         #Save2
         try:        
