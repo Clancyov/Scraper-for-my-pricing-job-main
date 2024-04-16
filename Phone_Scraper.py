495e92f031a59b858491c968e9ae24833d96c355
Amin Achak <82500146+Clancyov@users.noreply.github.com>
2024-04-16 21:40:50 +0330

    Update Phone_Scraper.py

--- Phone_Scraper.py
+++ Phone_Scraper.py
@@ -3,8 +3,20 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 logger = logging.getLogger(__name__)
+
 class Phone_Scraper :
 
+    def __init__(self):
+        # Define the directory structure
+        self.output_directory = "Outputs"
+        self.phone_directory = os.path.join(self.output_directory, "Phone")
+        self.scraped_data_directory = os.path.join(self.phone_directory, "Scraped_data")
+
+        # Create the directories if they don't exist
+        os.makedirs(self.output_directory, exist_ok=True)
+        os.makedirs(self.phone_directory, exist_ok=True)
+        os.makedirs(self.scraped_data_directory, exist_ok=True)
+        
     def Scrap ( self, Now) :
         try:
             # link to the webpage which data is there
@@ -47,7 +59,9 @@ class Phone_Scraper :
         
         try:
             # saving the output in a textfile
-            with open(f"Outputs\\Phones\\Scraped_data\\Scraped_data-{Now}.txt", 'w', encoding='utf-8') as File:
+            file_name = f"Scraped_data-{Now}.txt"
+            file_path = os.path.join(self.scraped_data_directory, file_name)
+            with open(file_path, 'w', encoding='utf-8') as File:
                 for data in All_Data:
                     File.write(data)
         except:
