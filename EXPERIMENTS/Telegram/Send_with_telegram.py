4c33e829d4a710ea93df6fcbdd811c3fe3273d59
Amin Achak <82500146+Clancyov@users.noreply.github.com>
2024-04-22 21:51:57 +0330

    Update Send_with_telegram.py

--- EXPERIMENTS/Telegram/Send_with_telegram.py
+++ EXPERIMENTS/Telegram/Send_with_telegram.py
@@ -1,35 +1,29 @@
-from telethon.sync import TelegramClient
-from secrets_1 import secrets
 import os
+from telegram import Bot
+from telegram.error import TelegramError
+# github_username = os.environ.get('GITHUB_USERNAME')
+# Your Telegram bot token
+TOKEN = os.environ.get('TELEGRAM_TOKEN')
 
-# Set up the Telegram client with your own account credentials
-api_id = secrets.api_id
-api_hash = secrets.api_hash
-phone_number = secrets.phonenumber
+# ID of the chat where you want to send the files
+CHAT_ID = os.environ.get('CHAT_ID')
 
-client = TelegramClient('send things', api_id, api_hash)
+# Directory containing the files you want to send
+FILES_DIRECTORY = 'Outputs/Phones/Images'
 
-async def main():
-    # Log in to your Telegram account
-    await client.start(phone_number)
+def send_files(bot, chat_id, files_directory):
+    for filename in os.listdir(files_directory):
+        file_path = os.path.join(files_directory, filename)
+        try:
+            with open(file_path, 'rb') as file:
+                bot.send_document(chat_id=chat_id, document=file)
+                print(f"File '{filename}' sent successfully")
+        except TelegramError as e:
+            print(f"Failed to send file '{filename}': {e}")
 
-    # Replace 'group_username' with the username of your group
-    group_id = -4119703192
-    group_entity = await client.get_entity(group_id)
+def main():
+    bot = Bot(TOKEN)
+    send_files(bot, CHAT_ID, FILES_DIRECTORY)
 
-    # Path to the folder containing images
-    folder_path = 'C:\\Users\\Achak\\Pictures\\Screenshots'
-
-    # Iterate through the images in the folder
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.jpg') or filename.endswith('.png'):
-            image_path = os.path.join(folder_path, filename)
-
-            # Send the image to the group
-            await client.send_file(group_entity, file=image_path)
-
-    print("All images sent to the group.")
-
-# Run the main function
-with client:
-    client.loop.run_until_complete(main())
+if __name__ == '__main__':
+    main()
