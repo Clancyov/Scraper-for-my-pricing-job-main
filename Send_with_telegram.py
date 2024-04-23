8cb95603d278f78fb7880bb1599ac692e9d9c797
Amin Achak <82500146+Clancyov@users.noreply.github.com>
2024-04-23 10:36:45 +0330

    Update Send_with_telegram.py

--- Send_with_telegram.py
+++ Send_with_telegram.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import telegram
 
@@ -8,16 +9,21 @@ print("token:  ", TOKEN)
 print("chat_id:  ", chat_id)
 bot = telegram.Bot(token=TOKEN)
 
-def send_files_in_directory(directory, chat_id):
-    for filename in os.listdir(directory):
-        file_path = os.path.join(directory, filename)
-        with open(file_path, 'rb') as file:
-            bot.send_document(document=file, chat_id=chat_id)
-            print(f"File '{filename}' sent successfully")
+async def send_document(document, chat_id):
+    async with bot:
+        await bot.send_document(document=document, chat_id=chat_id)
 
-def main():
-    # Sending all files in the directory
-    send_files_in_directory('Outputs/Phones/Images', chat_id)
+async def send_all_documents_in_directory(directory, chat_id):
+    async with bot:
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
+            with open(file_path, 'rb') as file:
+                await bot.send_document(document=file, chat_id=chat_id)
+                print(f"File '{filename}' sent successfully")
+
+async def main():
+    # Sending all documents in the directory
+    await send_all_documents_in_directory('Outputs/Phones/Images', chat_id)
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
