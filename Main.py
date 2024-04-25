1e4a62ba7b77e8adee9079264d5279327bd3d0ac
Amin Achak <82500146+Clancyov@users.noreply.github.com>
2024-04-25 11:51:01 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -28,14 +28,14 @@ class main:
     def Make_Car_Pricelists(self):
         pass
 
-    def Send_Outputs_With_Telegram(self):
+    # def Send_Outputs_With_Telegram(self):
         
-        Sender = Sender()
-        Sender.Send(Now)
+    #     Sender = Sender()
+    #     Sender.Send(Now)
     
 Runner=main()
 Runner.Make_Iron_Pricelists()
 Runner.Make_Phone_Pricelists()
-Runner.Send_Outputs_with_Telegram()
+# Runner.Send_Outputs_with_Telegram()
 
 Logger.info('Finished')
