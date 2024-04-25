6da10900d56f4c3961b55dbaaee276cba07cbed8
Clancyov <aminachak8182@gmail.com>
2024-04-25 11:59:11 +0330

    telegram added

---
+++ Telegram_Sender.py
@@ -0,0 +1,32 @@
+import asyncio
+import os
+import telegram
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Sender:
+    
+    TOKEN = os.environ.get('TELEGRAM_TOKEN')
+    chat_id = os.environ.get('CHAT_ID')
+
+    bot = telegram.Bot(token=TOKEN)
+
+    async def send_document(self, document):
+        async with self.bot:
+            await self.bot.send_document(document=document, chat_id=self.chat_id)
+
+    async def Send_Directory_Containing(self, directory):
+        async with self.bot:
+            for filename in os.listdir(directory):
+                file_path = os.path.join(directory, filename)
+                with open(file_path, 'rb') as file:
+                    await self.bot.send_document(document=file)
+
+    async def runner(self,Now):
+        # Sending all documents in the directory
+        await self.Send_Directory_Containing('Outputs/Phones/Images')
+
+        await self.Send_Directory_Containing(f"Outputs/Iron/Images/{Now}/Akhbar_Eghtesadi")
+
+        await self.Send_Directory_Containing(f"Outputs/Iron/Images/{Now}/Currency_Gold")
