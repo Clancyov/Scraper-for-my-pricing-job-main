aa0570987950c2d8a0919378117ad6810c065db4
Amin Achak <82500146+Clancyov@users.noreply.github.com>
2024-04-26 18:06:25 +0330

    Update Telegram_Sender.py

--- Telegram_Sender.py
+++ Telegram_Sender.py
@@ -24,15 +24,17 @@ class Sender:
                     await self.bot.send_document(document=file,chat_id=self.chat_id)
 
     async def runner(self,Now):
-        try:
+        try:      
+            await self.Send_Directory_Containing(f"Outputs/Iron/Images/{Now}/Currency_Gold")
+
+            await self.Send_Directory_Containing(f"Phones/Images/{Now}")
+        
             # Sending all documents in the directory
             await self.send_document(f'Log/Main_Logs/{Now}.log')
 
             await self.Send_Directory_Containing(f'Outputs/Iron/Images/{Now}/Akhbar_Eghtesadi')
 
-            await self.Send_Directory_Containing(f"Outputs/Iron/Images/{Now}/Currency_Gold")
 
-            await self.Send_Directory_Containing(f"Phones/Images/{Now}")
         except:
             logger.critical('Files Didnt sent')
         else:
