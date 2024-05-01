8a81c6e7b90f800702b1e74e02d1a8a382d65c30
Amin Achak <82500146+Clancyov@users.noreply.github.com>
2024-05-02 02:10:59 +0330

    Update Iron_Data_Processor.py

--- Iron_Data_Processor.py
+++ Iron_Data_Processor.py
@@ -185,7 +185,7 @@ class Iron_Data_Processor :
                 if not os.path.exists(Path):
                     os.makedirs(Path)
                 file_path = os.path.join(Path, f"{Page.Name}.png")
-                Raw_Canvas_Image= Raw_Canvas_Image.resize((540,540), resample=Image.NEAREST)
+                Raw_Canvas_Image= Raw_Canvas_Image.resize((720,720), resample=Image.NEAREST)
                 Raw_Canvas_Image.save(file_path)
         except:
             logger.critical('Couldent Make Images')
